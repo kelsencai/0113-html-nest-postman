3ed5ffa25bf8d0c954941435aba0d44e9cbf694d
kelsencai <59015674+kelsencai@users.noreply.github.com>
2025-01-13 16:10:02 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public')); // 提供静态文件服务
+  app.useStaticAssets(join(__dirname, '..', 'public')); // 启用静态文件服务
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
